e30300aefb83d621dbbc188cb8a9928803e3e111
Oliver Jean Eifler <olli.eifler@gmail.com>
2016-03-06 20:45:17 +0100

    ollicon
    replaced grunticon with ollicon (modified grunticon)

--- gruntfile.js
+++ gruntfile.js
@@ -5,7 +5,17 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         dir: {
             release: 'web',
+            build: 'build',
             assets: 'web/_assets'
+        },
+        //copy files
+        copy: {
+          icons: {
+              files:[
+                {expand: true,cwd: '<%= dir.build %>/icons',src: ['**/*.png'],dest: '<%= dir.release %>/css'},
+                  {expand: true,cwd: '<%= dir.build %>/icons',src: ['*.js'],dest: '<%= dir.release %>/js'},
+                  {expand: true,cwd: '<%= dir.build %>/icons',src: ['*.scss'],dest: '<%= dir.assets %>/sass/icons'}
+          ]}
         },
         /* Compile SASS to CSS */
         sass: {
@@ -20,15 +30,19 @@ module.exports = function (grunt) {
             }
         },
         /*GRUNTICON*/
-        grunticon: {
-            myIcons: {
+        ollicon: {
+            icons: {
                 files: [{
                     expand: true,
                     cwd: '<%= dir.assets %>/icons/source',
                     src: ['*.svg', '*.png'],
-                    dest: '<%= dir.release %>/css/icons'
+                    dest: '<%= dir.build %>/icons'
                 }],
                 options: {
+                    datasvgcss: '_svg.scss',
+                    datapngcss: '_png.scss',
+                    urlpngcss:  '_fallback.scss',
+                    pngfolder: 'icons/',
                     enhanceSVG: false,
                     compressPNG:true
                 }
@@ -50,8 +64,10 @@ module.exports = function (grunt) {
     });
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-grunticon');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadTasks('grunt-tasks/ollicon');
     // Default task(s).
     grunt.registerTask('css-d', ['sass']);
     grunt.registerTask('default', ['css-d']);
+    grunt.registerTask('icons', ['ollicon:icons','copy:icons']);
 };
